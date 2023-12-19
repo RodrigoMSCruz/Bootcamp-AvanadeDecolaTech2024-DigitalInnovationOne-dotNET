c187ee70f12b440d54078f214cca8799ffb7e10d
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-12-19 09:48:42 -0300

    2023-12-19 - Desafio - Criando um Sistema e Abstraindo um Celular com POO

---
+++ D02-Desafio/trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "22222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
