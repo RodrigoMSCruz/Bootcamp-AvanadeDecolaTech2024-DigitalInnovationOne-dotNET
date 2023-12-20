9ab9bff140dde28b145fc68a2d6ae99a783ccc25
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-12-19 22:15:23 -0300

    2023-12-19 - Testes Unitários em C# (2)

--- P04-ModuloTestes/CalculadoraTestes/CalculadoraTests.cs
+++ P04-ModuloTestes/CalculadoraTestes/CalculadoraTests.cs
@@ -15,7 +15,30 @@ public class CalculadoraTests
     public void DeveSomar5Com10ERetornar15()
     {
         // Arrange
+        int num1 = 5;
+        int num2 = 10;
+
         // Act
+        int resultado = _calc.Somar(num1, num2);
+        
         // Assert
+        Assert.Equal(15, resultado);
+        Assert.Equal( 15, _calc.Somar(5,10) );
+    }
+
+    
+    [Fact]
+    public void DeveSomar10Com10ERetornar20()
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 10;
+
+        // Act
+        int resultado = _calc.Somar(num1, num2);
+        
+        // Assert
+        Assert.Equal(20, resultado);
+        Assert.Equal( 20, _calc.Somar(10,10) );
     }
 }
